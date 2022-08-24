d44ca03ad6188fdb08e6e456a32900c2fe1fc649
oldman <itoldman@qq.com>
2022-08-24 14:53:43 +0800

    1

--- src/main/java/com/example/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkins/JenkinsDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
 
 }
