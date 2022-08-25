ad49d1b455e0f8ed60ede00993b6ff1a8dddf623
oldman <itoldman@qq.com>
2022-08-25 10:32:39 +0800

    1

---
+++ src/main/java/com/example/jenkins/ctrl/TestController.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.ctrl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String str(){
+        return "Hello Jenkins";
+    }
+}
